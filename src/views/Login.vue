0f7e2e8345588b2d6ac22a61f0447ec726f4f679
dbnorth <david.north@oc.edu>
2024-12-17 09:52:26 -0600

    Fix Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,50 +1,75 @@
 <script setup>
-import { ref } from "vue"
+import { ref } from "vue";
 import LoginDialog from "../components/LoginDialog.vue";
 
-const showLogin = ref(false)
+const showLogin = ref(false);
+const showSignup = ref(false);
 
 const changeLoginState = () => {
-  showLogin.value = !showLogin.value
-}
-
+  showLogin.value = !showLogin.value;
+};
+const changeSignupState = () => {
+  showSignup.value = !showSignup.value;
+};
 </script>
 
 <template>
-<div class="container" width="100%" height="100%">
-<v-container> 
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="10" md="8" lg="6" class="mt-16">
-      <v-card class="">
-        <v-card-title align="center">
-          Welcome to OneApp!
-        </v-card-title>
-        <v-card-subtitle align="center">
-          Your home for Church of Christ college applications.
-        </v-card-subtitle>
-        <v-card-text align="center">
-          <v-btn 
-            variant="outlined"
-            elevation="0"
-            density="comfortable" 
-            @click="changeLoginState"
-            color="primary"
-          >
-            Log In
-          </v-btn>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
-  
-</v-container>
+  <div class="container" width="100%" height="100%">
+    <v-container>
+      <v-row justify="center" align="center">
+        <v-col cols="12" sm="10" md="8" lg="6" class="mt-16">
+          <v-col class="">
+            <v-card-title align="center"> Welcome to OneApp! </v-card-title>
+            <v-card-subtitle align="center">
+              Your home for Church of Christ college applications.
+            </v-card-subtitle>
+            <v-row>
+              <v-col>
+                <v-card-text align="center">
+                  Sign up for an new account
+                </v-card-text>
+                <v-card-text align="center">
+                  <v-btn
+                    variant="outlined"
+                    elevation="0"
+                    density="comfortable"
+                    @click="changeSignupState"
+                    color="primary"
+                  >
+                    Sign Up
+                  </v-btn>
+                </v-card-text>
+              </v-col>
+              <v-col>
+                <v-card-text align="center">
+                  Login to your existing account
+                </v-card-text>
+                <v-card-text align="center">
+                  <v-btn
+                    variant="outlined"
+                    elevation="0"
+                    density="comfortable"
+                    @click="changeLoginState"
+                    color="primary"
+                  >
+                    Log In
+                  </v-btn>
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-col>
+      </v-row>
+    </v-container>
 
-<v-dialog v-model="showLogin" max-width="600px">
-  <LoginDialog 
-    @close-dialog="changeLoginState"
-  />
-</v-dialog>
-  
-</div>
-
+    <v-dialog v-model="showLogin" max-width="600px">
+      <LoginDialog :displayLoginFirst="true" @close-dialog="changeLoginState" />
+    </v-dialog>
+    <v-dialog v-model="showSignup" max-width="600px">
+      <LoginDialog
+        :displayLoginFirst="false"
+        @close-dialog="changeSignupState"
+      />
+    </v-dialog>
+  </div>
 </template>
